188dd94ab331ce0a1c3d2858dca02ffd6dab6f58
mafiuu <52745332+mafiuu@users.noreply.github.com>
2024-02-16 20:42:16 -0700

    2

--- SWConsole/GameActions.cs
+++ SWConsole/GameActions.cs
@@ -27,7 +27,7 @@ public class GameActions
             (Direction.Right, true) => heading + 10,
             (Direction.Right, false) => heading + 1,
             (Direction.Left, true) => heading - 10,
-            (Direction.Left, false) => heading - 1,
+            (Direction.Left, false) => heading - 20,
             _ => 0,//turn north if someone calls this with a bogus Direction
         };
         heading = ClampRotation(heading);
